8c1158c7a430886294c571dcb25f54843fbacb96
user <user@ws1013402134.pm>
2021-04-22 12:45:16 +0500

    aye

--- laba2.c
+++ laba2.c
@@ -2,15 +2,15 @@
 #include <math.h>
 int main()
 {
-    const int a = 2;
+    const double a = 2.0;
     double x;
     printf("Enter x:");
     scanf("%lf", &x);
     if ((x >= -1.8) && (x <= 1.8))
     {
-        double y = (pow(a, x) + pow(a, -x)) / 2;
+        double y = (pow(a, x) + pow(a, -x)) / 2.0;
         printf("y(x) = %lf\n", y);
-        double z = sqrt(2 + y - y * y);
+        double z = sqrt(2.0 + y - y * y);
         printf("z(y) = %lf\n", z);
 
     }
